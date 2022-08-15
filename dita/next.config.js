8c687a3ca995b9ef08c5e870da1aa498d1b75e3d
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-08-15 18:34:32 +0800

    taking a break - push zhor siin

--- dita/next.config.js
+++ dita/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
   reactStrictMode: true,
+
+  images: {
+    // accepted domains for pictures
+    domains: ['links.papareact.com']
+  }
 }

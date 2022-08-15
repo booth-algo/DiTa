1bfee1bfb1d649b168711b4fa9592b9dbb219f94
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-08-15 21:56:51 +0800

    Update index.tsx

--- dita/pages/index.tsx
+++ dita/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
-import Header from '../components/header'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (

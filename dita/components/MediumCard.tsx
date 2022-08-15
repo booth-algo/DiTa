728cecd80a4e0cd25743107199797ef0c6385c6e
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-08-15 23:46:47 +0800

    smallcardcomponent

---
+++ dita/components/MediumCard.tsx
@@ -0,0 +1,14 @@
+
+
+function MediumCard({img, title}) {
+
+    return (
+
+        <div>
+            {/* first video - 2:02:08 */}
+
+        </div>
+    )
+}
+
+export default MediumCard
--- dita/pages/index.tsx
+++ dita/pages/index.tsx
@@ -3,8 +3,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import SmallCard from '../components/SmallCard'
+import MediumCard from '../components/MediumCard'
 
-const Home: NextPage = () => {
+// remove exploreData when necessary 
+
+const Home: NextPage = ({ exploreData, cardsData }) => {
   return (
     <div className="" /*"flex min-h-screen flex-col items-center justify-center py-2"*/ >
       <Head>
@@ -28,10 +32,23 @@ const Home: NextPage = () => {
           {/* pb = padding bottom */}
 
           {/* Pull some data from a server - API endpoints */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {exploreData?.map((item) => (
+            <SmallCard key={item.img} img={item.img} distance={item.distance} location={item.location}/>
+          ))}
+        </div> 
 
+        </section>
 
+        <section>
+            <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+            <h2>
+              {cardsData.map(item => {
+                <MediumCard key={item.img} img={item.img} title={item.title}/>
 
+              })}
 
+            </h2>
         </section>
 
       </main>
@@ -42,6 +59,22 @@ const Home: NextPage = () => {
 
 export async function getStaticProps() {
   // for server rendering
+  const exploreData = await fetch("https://links.papareact.com/pyp").
+  then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch('https://links.papareact.com/zp1').
+  then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData
+    }
+  }
 }
 
 export default Home

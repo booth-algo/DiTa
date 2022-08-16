8e81690aea1cb0283bc91b4852ebb82e3734e289
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-08-16 13:27:00 +0800

    pages created, implementing search

---
+++ dita/pages/search.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+function Search() {
+  return (
+    <div className='h-screen'>
+    <Header />
+        {/* Video 3 stopped at 1:08:54 */}
+    <Footer />
+    </div>
+  )
+}
+
+export default Search

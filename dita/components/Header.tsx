4a668f96df8bf0172f99bdefdde683440ca3642d
benedictlouie <34078915+benedictlouie@users.noreply.github.com>
2022-08-16 21:36:06 +0800

    responsive nav bar

--- dita/components/Header.tsx
+++ dita/components/Header.tsx
@@ -8,7 +8,6 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
 import { Calendar } from 'react-date-range';
 
-
 function Header() {
 
     // const renderThemeChanger = () => {
@@ -46,36 +45,38 @@ function Header() {
         key: 'selection'
     };
 
+    const [isOpen, setIsOpen] = useState(false); // responsive nav bar
 
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10"> 
         {/* md:px is the padding until a certain breakpoint */}
-
+        
             {/* left */}
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
-                <Image 
+                {/* <Image 
                     // DiTa picture here 
                     src='https://links.papareact.com/qd3'
                     layout='fill'
                     objectFit="contain"
                     objectPosition="left"
-                />
+                /> */}
+                <div className="font-bold text-3xl text-amber-600">Di</div><div className="text-3xl text-blue-500">Ta</div>
             </div>
 
             {/* middle */}
-            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+            {/* <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
                 <input 
                 value={searchInput}
                 onChange={(e) => setSearchInput(e.target.value)}
                 className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search"/>
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
-            </div>
+            </div> */}
 
             {/* right */}
             <div className="flex items-center space-x-4 justify-end text-gray-500">
                 {/* see which justify is better 
                 problem is when i hover over one of the buttons, the button semibolds and it moves the other buttons*/}
-                <div className="hidden lg:flex items-center space-x-5 px-5">
+                {/* <div className="hidden lg:flex items-center space-x-5 px-5">
                     <button className="hover:font-semibold ease-out active:scale-90 duration-150">Home</button>
 
                     <button className="hover:font-semibold ease-out active:scale-90 duration-150">Services</button>
@@ -83,7 +84,7 @@ function Header() {
                     <button className="hover:font-semibold ease-out active:scale-90 duration-150">About</button>
 
                     <button className="hover:font-semibold ease-out active:scale-90 duration-150">Contact</button>
-                </div>
+                </div> */}
 
                 {/* Language Icon */}
                 <button className="ease-out active:scale-90 duration-150"><TranslateIcon className="h-6"/></button> 
@@ -92,11 +93,24 @@ function Header() {
                 <button className="ease-out active:scale-90 duration-150"><SunIcon className="h-6"/></button>
 
 
-                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-                    <MenuIcon className="h-6"/>
+                {/* <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                    <MenuIcon className="h-6"/> */}
                     {/* <UserCircleIcon className="h-6"/> */}
-                </div>
+                {/* </div> */}
 
+            </div>
+
+            {/* responsive nav bar */}
+            <div id="navBar">
+                <div id="items" className={isOpen && 'open'}>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+                </div>
+                <div id="navToggle" className={isOpen && 'open'} onClick={() => setIsOpen(!isOpen)}>
+                    <div id="bar"></div>
+                </div>
             </div>
 
             {searchInput && 

f33fe209f862b831a0c326746b8b1801a98257b1
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-08-19 23:35:10 +0800

    this progress bar looks sick as fuck

--- dita/pages/_app.tsx
+++ dita/pages/_app.tsx
@@ -1,5 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from "next/router";
+
+
+
+// npm i @badrap/bar-of-progress HOLY FUCK THIS LOOKS SO COOL
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+  // lol the delay XD
+
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
+
+// end of progress bar
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

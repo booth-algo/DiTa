39863a13f73143df73425c1bcb0dd7738b95a4be
benedictlouie <34078915+benedictlouie@users.noreply.github.com>
2022-08-21 22:01:30 +0800

    about, contact

--- dita/pages/aboutUs.tsx
+++ dita/pages/aboutUs.tsx
@@ -6,8 +6,32 @@ function aboutUs() {
   return (
     <div>
       <Header />
+      
+      <div style={{height: '800px', backgroundColor: '#999'}}>
+      
+      <div style={{position: 'absolute', top: '80px', left: '0'}} className="w-0 h-0 
+      border-t-[800px] border-t-transparent
+      border-l-[800px] border-l-blue-800
+      border-b-[0px] border-b-transparent
+      "></div>
 
-      <div className='pt-10 pb-10'>About Us</div>
+      <div style={{position: 'absolute', top: '80px', left: '0'}} className="w-0 h-0 
+      border-t-[0px] border-t-transparent
+      border-l-[400px] border-l-blue-900
+      border-b-[400px] border-b-transparent
+      "></div>
+
+      <div style={{position: 'absolute', top: '250px', left:'50px'}} className="text-white text-7xl font-extrabold">About DiTa</div>
+
+      <div style={{position: 'absolute', top: '500px', left:'50px', width:'300px'}} className="text-white text-base">Lorem ipsum dolor sit amet. Est quae porro ea corporis perspiciatis aut voluptatibus alias et velit possimus quo suscipit fugiat id quia commodi ut aperiam? Est quidem illo eum laudantium unde id nihil natus. Non perspiciatis temporibus in ducimus corrupti ea unde amet est cumque neque quo dolorem nisi qui laborum officia ad molestias sint.</div>
+
+      </div>
+
+      <div style={{padding: '50px'}}>
+      Ut tenetur debitis et perferendis debitis vel aperiam enim aut tempore necessitatibus in magnam quidem ab accusantium galisum. Et dolorem velit sed tempore eius aut cupiditate maiores est neque repellat tempora minima. Eum quae galisum eos porro voluptatum et repellendus maiores ut debitis iusto. Et velit dolor et praesentium nemo ut exercitationem dolores et fugit sunt.
+      <br /><br />
+      Aut aspernatur blanditiis 33 enim vero et praesentium quidem ea labore fugiat ut velit architecto aut voluptates numquam eum dolore molestias. Aut odit repellendus ex fugit nulla et tempore deleniti ad quia aperiam qui quasi dolorum eum delectus ipsam.
+      </div>
 
 
       <Footer />
--- dita/pages/contactUs.tsx
+++ dita/pages/contactUs.tsx
@@ -7,7 +7,24 @@ function contactUs() {
     <div>
       <Header />
 
-      <div className='pt-10 pb-10'>Contact Us</div>
+      {/* <div className='pt-10 pb-10'>Contact Us</div> */}
+
+      <div style={{padding: '50px', height: '300px'}} className="bg-gradient-to-r from-indigo-600 via-indigo-500 to-cyan-500 h-80 block">
+      <div className="text-white text-7xl font-extrabold"><br />Contact us</div>
+      </div>
+
+      <br />
+      <div style={{paddingLeft: '50px'}}>
+      <span className="text-3xl">📞</span> &nbsp; +852 1234 5678
+      </div><br />
+      <div style={{paddingLeft: '50px'}}>
+      <span className="text-3xl">✉️</span> &nbsp; abc@gmail.com
+      </div><br />
+      <div style={{paddingLeft: '50px'}}>
+      <span className="text-3xl">📍</span> &nbsp; Somewhere in the world
+      </div><br />
+
+      <br /><br />
 
       <Footer />
     </div>

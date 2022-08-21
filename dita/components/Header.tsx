e1d77c8c0623cc794a0f0347d097c8efc6e8d2b4
benedictlouie <34078915+benedictlouie@users.noreply.github.com>
2022-08-21 19:14:53 +0800

    nav bar fix

--- dita/components/Header.tsx
+++ dita/components/Header.tsx
@@ -81,14 +81,14 @@ function Header({ placeholder }) {
             </div>
 
             {/* middle */}
-            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+            {/* <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
                 <input 
                 value={searchInput}
                 onChange={(e) => setSearchInput(e.target.value)}
                 className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" 
                 placeholder={placeholder || "Start your search"}/>
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
-            </div> 
+            </div>  */}
 
             {/* right */}
             <div className="flex items-center space-x-4 justify-end text-gray-500">
@@ -104,12 +104,6 @@ function Header({ placeholder }) {
                     <button className="hover:font-semibold ease-out active:scale-90 duration-150">Contact</button>
                 </div> */}
 
-                {/* Language Icon */}
-                <button className="ease-out active:scale-90 duration-150"><TranslateIcon className="h-6"/></button> 
-
-                {/* Light Mode, Dark Mode */}
-                <button className="ease-out active:scale-90 duration-150"><SunIcon className="h-6"/></button>
-
 
                 {/* <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
                     <MenuIcon className="h-6"/> */}
@@ -120,15 +114,24 @@ function Header({ placeholder }) {
 
             {/* responsive nav bar */}
             <div id="navBar">
-                <div id="items" className={isOpen && 'open'}>
+                <div id="items" className={isOpen && 'open'} style={{position:'absolute', right:'100px'}}>
                     <a onClick={() => router.push("/")} >Home</a>
                     <a onClick={() => router.push("services")}>Services</a>
                     <a onClick={() => router.push("aboutUs")}>About</a>
                     <a onClick={() => router.push("contactUs")}>Contact</a>
+                    &nbsp;&nbsp;
                 </div>
-                <div id="navToggle" className={isOpen && 'open'} onClick={() => setIsOpen(!isOpen)}>
-                    <div id="bar"></div>
+
+                <div style={{position:'absolute', right:'10px'}} className="flex space-x-6 justify-end text-gray-500">
+                    {/* Language Icon */}
+                    <button className="ease-out active:scale-90 duration-150"><TranslateIcon className="h-6"/></button> 
+                    {/* Light Mode, Dark Mode */}
+                    <button className="ease-out active:scale-90 duration-150"><SunIcon className="h-6"/></button>
+                    <div id="navToggle" className={isOpen && 'open'} onClick={() => setIsOpen(!isOpen)}>
+                        <div id="bar" className="h-6"></div>
+                    </div>
                 </div>
+
             </div>
 
             {searchInput && 

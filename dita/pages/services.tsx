a326bbcef66c91d8c291bc901ad8340a18351894
benedictlouie <34078915+benedictlouie@users.noreply.github.com>
2022-08-24 17:41:45 +0800

    edit

--- dita/pages/services.tsx
+++ dita/pages/services.tsx
@@ -7,7 +7,15 @@ function services() {
     <div>
       <Header />
       
-      <h1 className='px-10 pt-10 pb-10 text-4xl'>Services</h1>
+      {/* <h1 className='px-10 pt-10 pb-10 text-4xl'>Services</h1> */}
+
+      <div style={{padding: '50px', height: '300px'}} className="bg-gradient-to-r from-red-600 via-orange-500 to-yellow-600 h-80 block">
+      <div className="text-white text-6xl font-extrabold"><br />Services</div>
+      </div>
+
+      <div style={{padding: '50px'}}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
       
       <Footer />
       </div>

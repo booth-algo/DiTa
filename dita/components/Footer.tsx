0001580d3da5b899578792cabaf9c1fec2210829
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-08-27 18:26:00 +0800

    fixing footer

--- dita/components/Footer.tsx
+++ dita/components/Footer.tsx
@@ -1,51 +1,36 @@
+import router from "next/dist/client/router";
 
 function Footer() {
     return (
-    // <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
-    //     <div className="space-y-4 text-xs text-gray-800">
-    //         <h5 className="font-bold">ABOUT</h5>
-    //         <p>How Airbnb works</p>
-    //         <p>Newsroom</p>
-    //     </div>
-
-    //     <div className="space-y-4 text-xs text-gray-800">
-    //         <h5 className="font-bold">CONTACT</h5>
-    //         <p>How Airbnb works</p>
-    //         <p>Newsroom</p>
-    //     </div>
-
-    //     <div className="space-y-4 text-xs text-gray-800">
-    //         <h5 className="font-bold">3</h5>
-    //         <p>How Airbnb works</p>
-    //         <p>Newsroom</p>
-    //     </div>
-
-    //     <div className="space-y-4 text-xs text-gray-800">
-    //         <h5 className="font-bold">CONTACT</h5>
-    //         <p>How Airbnb works</p>
-    //         <p>Newsroom</p>
-    //     </div>
-
-    // </div>
     
-    <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
-        <div className="space-y-4 text-xs text-gray-800">
-            <h5 className="font-bold">ABOUT</h5>
+    <main>
+        <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 onClick={() => router.push("/")} className="font-bold hover:scale-110 cursor-pointer">HOME</h5>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 onClick={() => router.push("services")} className="font-bold hover:scale-110 cursor-pointer">SERVICES</h5>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 onClick={() => router.push("aboutUs")} className="font-bold hover:scale-110 cursor-pointer">ABOUT</h5>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 onClick={() => router.push("contactUs")} className="font-bold hover:scale-110 cursor-pointer">CONTACT</h5>
+            </div>
+
         </div>
 
-        <div className="space-y-4 text-xs text-gray-800">
-            <h5 className="font-bold">SERVICES</h5>
+        <div>
+
+
+            
         </div>
 
-        <div className="space-y-4 text-xs text-gray-800">
-            <h5 className="font-bold">ABOUT</h5>
-        </div>
 
-        <div className="space-y-4 text-xs text-gray-800">
-            <h5 className="font-bold">CONTACT</h5>
-        </div>
-
-    </div>
+    </main>
     
     
     );

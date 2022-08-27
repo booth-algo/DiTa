841f79a2e098beea4535f9a59dc487da2490a50a
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-08-27 22:05:02 +0800

    naisu footer

--- dita/components/Footer.tsx
+++ dita/components/Footer.tsx
@@ -1,5 +1,5 @@
 import router from "next/dist/client/router";
-import { SunIcon, MailIcon, PhoneIcon } from '@heroicons/react/outline';
+import { SunIcon, MailIcon, PhoneIcon, ChatIcon, DeviceMobileIcon,  } from '@heroicons/react/outline';
 
 
 function Footer() {
@@ -7,7 +7,7 @@ function Footer() {
     
     <main className="bg-gray-100 text-gray-600">
 
-        <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14">
+        <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 pt-14">
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 onClick={() => router.push("/")} className="font-bold hover:scale-110 cursor-pointer">HOME</h5>
             </div>
@@ -26,12 +26,16 @@ function Footer() {
 
         </div>
 
-        <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14">
+        <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 pt-14">
             <button className="ease-out active:scale-90 duration-150"><MailIcon className="h-6"/></button> 
+            <button className="ease-out active:scale-90 duration-150"><ChatIcon className="h-6"/></button> 
+            <button className="ease-out active:scale-90 duration-150"><PhoneIcon className="h-6"/></button> 
+            <button className="ease-out active:scale-90 duration-150"><DeviceMobileIcon className="h-6"/></button> 
 
+        </div>
 
-
-
+        <div className="justify-items-center grid grid-cols-1 gap-y-10 px-32 py-14">
+            <h1>©DiTa Limousine Ltd.</h1>
         </div>
 
 

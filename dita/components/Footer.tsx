67bfb697737f9c9498b696dde45151683b151a9c
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-08-27 18:33:44 +0800

    updated footer

--- dita/components/Footer.tsx
+++ dita/components/Footer.tsx
@@ -1,10 +1,13 @@
 import router from "next/dist/client/router";
+import { SunIcon, MailIcon, PhoneIcon } from '@heroicons/react/outline';
+
 
 function Footer() {
     return (
     
-    <main>
-        <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+    <main className="bg-gray-100 text-gray-600">
+
+        <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14">
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 onClick={() => router.push("/")} className="font-bold hover:scale-110 cursor-pointer">HOME</h5>
             </div>
@@ -23,10 +26,12 @@ function Footer() {
 
         </div>
 
-        <div>
+        <div className="justify-items-center grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14">
+            <button className="ease-out active:scale-90 duration-150"><MailIcon className="h-6"/></button> 
 
 
-            
+
+
         </div>
 
 

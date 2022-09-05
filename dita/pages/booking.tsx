7641e916ac74e1b98e8642ee064b7c86654eb08b
Kevin Lau <107279223+s-d-y-y@users.noreply.github.com>
2022-09-06 00:35:41 +0800

    literally nothing in here

--- dita/pages/booking.tsx
+++ dita/pages/booking.tsx
@@ -78,7 +78,7 @@ function booking({ placeholder }) {
 
                 <form action="" className='flex flex-col space-y-4'>
                   <div>
-                    <label htmlFor="" className='text-sm'>Your name</label>
+                    <label htmlFor="" className='text-sm'>Your name</label>    
                     <input type="text" placeholder='Kimi no nawa' className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300' />
                   </div>
       

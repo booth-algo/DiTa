4184710ffdc5e8d96c8250e82c721dde33c27475
iamkenlau <121297532+iamkenlau@users.noreply.github.com>
2022-12-23 13:06:27 +0000

    Update aboutUs.tsx

--- dita/pages/aboutUs.tsx
+++ dita/pages/aboutUs.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 function aboutUs() {
   return (
     <div>
-      <Header />
+      <Header /> 
       
       {/* <div className='pt-10 pb-10'>Contact Us</div> */}
 

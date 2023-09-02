1514aed2e7079681b941e34f1018212a74af8165
Kevin Lau <107279223+booth-algo@users.noreply.github.com>
2023-09-02 17:20:48 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,6 @@ const Footer = () => {
         <p className={`${styles.sectionSubText} text-center`}>
           DiTa Limousine Limited
         </p>
-        <p className={`sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider text-center`}>
-          Designed by ...
-        </p>
     </div>  
   )
 }
